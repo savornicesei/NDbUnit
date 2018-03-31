a5c230aa3d4742f16f1a2f2bb99339efc5bb146c
Simona Avornicesei <savornicesei@gmail.com>
2018-03-31 13:58:03 +0300

    #23 Update project targeted frameworks, tools and libraries

--- scripts/build-setup.ps1
+++ scripts/build-setup.ps1
@@ -6,6 +6,7 @@
      - downloading NUnit console runner
      - downloading OpenCover
      - downloading coveralls.net
+     - downloading SonarQube MSBuild scanner
      The script is agnostic to the platform it is run on (windows, linux or mac)
 .INPUTTYPE
    Documentary text, eg:
@@ -16,17 +17,36 @@
    Output type  [Universal.SolarSystem.Planetary.Wisdom]
    Appears in -full
 #>
+Import-Module -Name OneGet
 import-module PackageManagement
 
 function Build-Setup{
 	param
     (
-		[Parameter(Mandatory = $true, HelpMessage = "Some dummy parameter.")]
+		[Parameter(Mandatory = $true, HelpMessage = "Operating.")]
         [String]
-		$Name
+		$Os
 	)
-
 	
+}
 
+function Check-Machine{
+	$checkOk = true;
+	
+	if($PSVersionTable -eq $null )
+	{
+		$checkOk = false;
+		Write-Host -ForegroundColor Red "Powershell version should be 6 or greater. Please upgrade!"
+	}
+	
+	return $checkOk;
+}
 
+function Install-Choco{
+	param
+	{
+		[Parameter(Mandatory = $true, HelpMessage = "Operating system (can be Unix, Windows or .")]
+		[String]
+		$Os
+	}
 }
